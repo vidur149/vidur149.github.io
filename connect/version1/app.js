99428b925421851288a6377e972273cfb128f9a1
vidur <vidursingla13@gmail.com>
2017-05-23 21:43:26 +0530

    angular frontend

---
+++ connect/version1/app.js
@@ -0,0 +1,33 @@
+(function() {
+    angular.module('auth');
+    angular.module('Home');
+    angular.module("connect", [
+        'auth',
+        'Home',
+        'ngRoute'
+    ]);
+
+    angular.module("connect").config(config);
+
+    function config($routeProvider, $qProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $qProvider.errorOnUnhandledRejections(false);
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'modules/auth/login.html',
+                controllerAs: 'vm'
+            })
+            .when('/home', {
+                controller: 'HomeController',
+                templateUrl: 'modules/home/home.html',
+                controllerAs: 'vm'
+            })
+            .when('/signup', {
+                controller: 'SignupController',
+                templateUrl: 'modules/auth/signup.html',
+                controllerAs: 'vm'
+            })
+            .otherwise({ redirectTo: '/' });
+    }
+})();
