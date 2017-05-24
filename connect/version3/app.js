cb962dc95966630a3735667e54593ef14bcfca67
vidur <vidursingla13@gmail.com>
2017-05-24 21:08:15 +0530

    version2 angular files

---
+++ connect/version3/app.js
@@ -0,0 +1,40 @@
+(function() {
+    angular.module('auth');
+    angular.module('Home');
+    // angular.module('angularFileUpload');
+    angular.module("connect", [
+        'auth',
+        'Home',
+        'ngRoute',
+        // 'angularFileUpload'
+    ]);
+
+    angular.module("connect").config(config);
+
+    function config($routeProvider, $qProvider, $locationProvider) {
+        $locationProvider.hashPrefix('');
+        $qProvider.errorOnUnhandledRejections(false);
+        $routeProvider
+            .when('/', {
+                controller: 'LoginController',
+                templateUrl: 'modules/auth/login.html',
+                controllerAs: 'vm'
+            })
+            .when('/home', {
+                controller: 'HomeController',
+                templateUrl: 'modules/home/home.html',
+                controllerAs: 'vm'
+            })
+            .when('/home/me', {
+                controller: 'ProfileController',
+                templateUrl: 'modules/home/profile.html',
+                controllerAs: 'vm'
+            })
+            .when('/signup', {
+                controller: 'SignupController',
+                templateUrl: 'modules/auth/signup.html',
+                controllerAs: 'vm'
+            })
+            .otherwise({ redirectTo: '/' });
+    }
+})();
---
+++ connect/version3/modules/home/service.js
@@ -0,0 +1,75 @@
+(function() {
+    angular.module('Home').factory('TimelineService', TimelineService);
+    TimelineService.$inject = ['$http', '$rootScope', '$cookieStore', '$q'];
+
+    function TimelineService($http, $rootScope, $cookieStore, $q) {
+        service = {};
+        service.GetPosts = function() {
+            var defer = $q.defer();
+            $http.get('http://localhost:5000/allposts').then(function(response) {
+                defer.resolve(response.data['posts']);
+            });
+            return defer.promise;
+        }
+        service.GetMyPosts = function() {
+            var defer = $q.defer();
+            $http.get('http://localhost:5000/posts').then(function(response) {
+                defer.resolve(response.data['posts']);
+            });
+            return defer.promise;
+        }
+
+        service.PostPosts = function(content) {
+            var defer = $q.defer();
+            $http.post('http://localhost:5000/posts', { content: content }).then(function(response) {
+                console.log(response);
+                defer.resolve(response.data);
+            }, function(error) {
+                defer.resolve(error);
+            });
+            return defer.promise;
+        }
+
+        service.Like = function(postid) {
+            var defer = $q.defer();
+            $http.post('http://localhost:5000/posts/' + postid + '/like').then(function(response) {
+                defer.resolve(response.data['likes']);
+            }, function(error) {
+                console.log(error);
+            });
+            return defer.promise;
+        }
+
+        service.GetUser = function() {
+            user = $cookieStore.get('globals');
+            return user['currentUser']['username'];
+        }
+        service.SetCredentials = function() {
+            user = $cookieStore.get('globals');
+            authdata = user['currentUser']['authdata'];
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+        };
+
+        service.Comment = function(content, postid) {
+            var defer = $q.defer();
+            $http.post('http://localhost:5000/posts/' + postid + '/comment', { content: content }).then(function(response) {
+                defer.resolve(response.data['comment']);
+            }, function(error) {
+                defer.resolve(error);
+            });
+            return defer.promise;
+        }
+
+        service.CommentOnComment = function(content, postid, commentid) {
+            var defer = $q.defer();
+            $http.post('http://localhost:5000/posts/' + postid + '/comment/' + commentid, { content: content }).then(function(response) {
+                defer.resolve(response.data['comment']);
+            }, function(error) {
+                defer.resolve(error);
+            });
+            return defer.promise;
+        }
+
+        return service;
+    }
+})();
