6d270a199182c370373befc1f5d92df028ec1e02
vidur <vidursingla13@gmail.com>
2017-06-17 19:11:18 +0530

    Modified ecom-1

--- angular/ecom1(ngClick)/app.js
+++ angular/ecom1(ngClick)/app.js
@@ -1,18 +1,12 @@
-(function() {
-    angular.module("ecom", []);
-    angular.module("ecom").controller("myCtrl", myCtrl);
-
-    function myCtrl() {
-        var vm = this;
-        vm.message = "Yeh message controller ke variable mein stored hai";
-        vm.backColor = "red";
-        vm.counter = 0;
-        vm.classChange = function() {
-            vm.backColor = vm.backColor === "red" ? "blue" : "red";
+angular.module('counter', [])
+    .controller("myCtrl", ["$scope", function($scope) {
+        $scope.message = "Yeh message controller ke variable mein stored hai";
+        $scope.backColor = "red";
+        $scope.counter = 0;
+        $scope.classChange = function() {
+            $scope.backColor = $scope.backColor === "red" ? "blue" : "red";
         }
-        vm.increment = function() {
-            vm.counter = vm.counter + 1;
+        $scope.increment = function() {
+            $scope.counter = $scope.counter + 1;
         };
-
-    }
-})(window.angular);
+    }]);
