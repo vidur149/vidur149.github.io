e7cd8680efa88d11b3f1b3462e73bff279b14e56
vidur149 <vidursingla13@gmail.com>
2020-02-07 18:24:15 +0530

    adds react sidebar

---
+++ react-sidebar/precache-manifest.bde88e05b12ac5d78a131bd49afb0f5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94026da839e213fa1ce3e0261dee73fd",
+    "url": "/react-sidebar/index.html"
+  },
+  {
+    "revision": "77fe445afe85de7c4546",
+    "url": "/react-sidebar/static/js/2.8ec18176.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/react-sidebar/static/js/2.8ec18176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae13a67af726199f73e8",
+    "url": "/react-sidebar/static/js/main.02249bf7.chunk.js"
+  },
+  {
+    "revision": "76d27c46328e48f2c99a",
+    "url": "/react-sidebar/static/js/runtime-main.51e74cfc.js"
+  },
+  {
+    "revision": "03bd9eda8bf01741d5f43c77de694735",
+    "url": "/react-sidebar/static/media/flat.03bd9eda.svg"
+  },
+  {
+    "revision": "24dc75263b41470074b98b7597f8876c",
+    "url": "/react-sidebar/static/media/happy.24dc7526.svg"
+  },
+  {
+    "revision": "b7bfb7a57fd093f567021e41e046d267",
+    "url": "/react-sidebar/static/media/sad.b7bfb7a5.svg"
+  }
+]);
